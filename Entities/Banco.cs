45ded16ee222c9ffad866f8162f4c70553bc4aa8
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-03-08 11:25:16 -0300

    Criação da classe Transferencia e Banco. Criação dos métodos e propriedades das classes

---
+++ Entities/Banco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Entities
+{
+    internal class Banco
+    {
+        public int NumeroAgencia { get; set; }
+        public string Nome { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public List<Conta> Contas { get; set; }
+
+        public Banco(int numero, string nome)
+        {
+            NumeroAgencia = numero;
+            Nome = nome;
+        }
+
+        public void CadastrarCliente(Cliente cliente)
+        {
+            Clientes.Add(cliente);
+        }
+
+        public void AbrirConta(Conta conta)
+        {
+            //verificar se é conta Corrente ou Poupanca ante de abrir e salvar cada tipo de conta ou invés de conta genérica
+            Contas.Add(conta);
+        }
+    }
+}
---
+++ Entities/Transferencia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Entities
+{
+    internal class Transferencia
+    {
+        public Conta ContaOrigem { get; set; }
+        public Conta ContaDestino { get; set; }
+        public double ValorTransferencia { get; set; }
+        
+        public Transferencia(Conta origem, Conta destino, double valor)
+        {
+            ContaOrigem = origem;
+            ContaDestino = destino;
+            ValorTransferencia = valor;
+        }
+
+        public void Transferir()
+        {
+            if (ContaOrigem.Saldo < ValorTransferencia)
+            {
+                Console.WriteLine("Saldo insuficiente para transferência");
+            }
+            else
+            {
+                ContaOrigem.TransferirValor(ValorTransferencia);
+                ContaDestino.Depositar(ValorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            return ContaOrigem + " " + ContaDestino + " " + ValorTransferencia;
+        }
+    }
+}
