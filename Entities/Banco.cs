1f265853294256610a668e800dc13d7bea784a6b
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-03-10 22:21:55 -0300

    Criação dos métodos Acessar Conta e Encontrar Conta. Instanciação das listas Cliente e Conta ao inicializar as variáveis/atributos

--- Entities/Banco.cs
+++ Entities/Banco.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SistemaBancario.Entities
 {
@@ -10,8 +11,8 @@ namespace SistemaBancario.Entities
     {
         public int NumeroAgencia { get; set; }
         public string Nome { get; set; }
-        public List<Cliente> Clientes { get; set; }
-        public List<Conta> Contas { get; set; }
+        public List<Cliente> Clientes { get; set; } = new List<Cliente>();
+        public List<Conta> Contas { get; set; } = new List<Conta>();
 
         public Banco(int numero, string nome)
         {
@@ -26,8 +27,39 @@ namespace SistemaBancario.Entities
 
         public void AbrirConta(Conta conta)
         {
-            //verificar se é conta Corrente ou Poupanca ante de abrir e salvar cada tipo de conta ou invés de conta genérica
             Contas.Add(conta);
+            Console.WriteLine("Conta criada com sucesso! \nNumero da conta: {0} Senha: {1} \nGuarde esses dados, eles não serão exibidos novamente", conta.Numero, conta.Senha);
+        }
+
+        public Conta AcessarConta(int numero, int senha)
+        {
+            Conta conta = null;
+
+            var filtrarConta = (from c in Contas
+                         where numero == c.Numero && senha == c.Senha
+                         select c);
+
+            foreach (Conta c in filtrarConta)
+            {
+                conta = c;
+            }
+
+            return conta;
+        }
+
+        public Conta EncontrarConta(int numero)
+        {
+            Conta conta = null;
+            var filtrarConta = (from c in Contas
+                                where numero == c.Numero
+                                select c);
+
+            foreach (Conta c in filtrarConta)
+            {
+                conta = c;
+            }
+
+            return conta;
         }
     }
 }
