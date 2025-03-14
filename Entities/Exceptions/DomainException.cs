5eee43f83bcefd94d6c2b75022bb1aa8f40690e7
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-03-13 21:52:28 -0300

    Criação da classe DomainException para tratar exceções da classe de domínio, classe contento os principais métodos de regra de negócio do sistema

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaBancario.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {        
+        }
+    }
+}
