c1866802eb862876091b07ab4d4037d5140faa99
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-03-13 21:54:32 -0300

    Criação da pasta para enumeradores e criacao do enumerador StatusConta para representar se a conta está ativa/manuteção/bloqueada

---
+++ Entities/Enums/StatusConta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Entities.Enums
+{
+    enum StatusConta : int
+    {
+        Ativa = 1,
+        Manutencao = 2,
+        Bloqueada = 3
+    }
+}
